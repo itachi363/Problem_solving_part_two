b9f03d9073073b25bd5c396319077297cd6269d0
itachi363 <itachi363@yahoo.com>
2022-03-01 11:07:53 -0500

    happy numbers

---
+++ problem_solving_part_two.py
@@ -0,0 +1,7 @@
+def happy_number(happy):
+    happy = input("Please enter a number to see if it is happy or sad: ")
+    for number in range(len(happy)):
+        if happy[number] is len(1)
+
+
+number_happy = happy_number()
