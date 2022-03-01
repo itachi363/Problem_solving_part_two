a2c43f940cc3178505a56e4ec11226082fab55a2
itachi363 <itachi363@yahoo.com>
2022-03-01 11:26:17 -0500

    basic happy number

--- problem_solving_part_two.py
+++ problem_solving_part_two.py
@@ -1,7 +1,11 @@
 def happy_number(happy):
     happy = input("Please enter a number to see if it is happy or sad: ")
     for number in range(len(happy)):
-        if happy[number] is len(1)
+        if int(happy) ** 2 == 1:
+            print("This is a happy number.")
+
+        else:
+            print("This is a sad number")
 
 
-number_happy = happy_number()
+number_happy = happy_number("")
